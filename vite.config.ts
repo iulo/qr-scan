00c7edee7c6ebb4e50d2851f15e59c3ce3c1edb5
iulo <liulong0130@gmail.com>
2022-05-23 01:02:01 +0800

    chore: build strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
   },
   base: '/',
   // base: '/qr-scan/',
-  plugins: [vue()]
+  plugins: [vue(), splitVendorChunkPlugin()]
 })
