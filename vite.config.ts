1659a3ba5d3db74fa0ff0f778216403fe380c07a
iulo <liulong0130@gmail.com>
2022-05-27 13:44:15 +0800

    refactor: pwa ui

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'maskable_icon_x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
